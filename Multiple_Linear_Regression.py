22a5db046475a81627306b3f6549475987c25f5d
NTHUMan <erichsieh081@gmail.com>
2018-04-29 20:02:55 +0800

    add backward elimination implementation, including automatic or artifical tuning

--- Multiple_Linear_Regression.py
+++ Multiple_Linear_Regression.py
@@ -9,7 +9,7 @@ Created on Sun Apr 29 12:39:26 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.cross_validation import train_set_split
+from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.linear_model import LinearRegression
 
@@ -35,3 +35,71 @@ regressor.fit(xtrain, ytrain)
 prediction = regressor.predict(xtest)
 
 
+# next is for backward elimination
+import statsmodels.formula.api as smodel
+#add the x0 from b0*x0 for the feature, x0 is always 1, b0*x0 is at the head of function
+#y = b0*x0 + b1*x1 ...
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+# X_ideal means the features that have high impact on prediction
+
+#auto tune ideal train feature
+newX = X[:,[0, 1, 2, 3, 4, 5]]
+X_ideal = newX[:,[0, 1, 2, 3, 4, 5]]
+regressor_OLS = smodel.OLS(endog = y, exog = X_ideal).fit()
+regressor_OLS.summary()
+type(regressor_OLS._results.pvalues)
+SL_value = 0.05
+flag = True
+level = 0
+
+while flag == True:
+    print(regressor_OLS._results.pvalues)
+    level+=1
+    flag = False
+    max_overflow = 0.0
+    max_index = -1
+    #print(regressor_OLS._results.pvalues.shape)
+    for index, p_value in enumerate(regressor_OLS._results.pvalues):
+        if p_value > SL_value:
+            #print("find ", level," ", p_value)
+            flag = True
+            if p_value > max_overflow:
+                max_overflow = p_value
+                max_index = index
+    
+    if max_index >= 0:
+        ask = list()
+        for i in range(0, regressor_OLS._results.pvalues.shape[0]):
+            if i != max_index:
+                ask.append(i)
+        print(ask)
+        X_ideal = newX[:, ask]
+        newX = newX[:, ask]
+    
+    regressor_OLS = smodel.OLS(endog = y, exog = X_ideal).fit()
+
+        
+# human tune ideal train feature
+# =============================================================================
+# X_ideal = X[:, [0, 1, 2, 3, 4, 5]]
+# regressor_OLS = smodel.OLS(endog = y, exog = X_ideal).fit()
+# regressor_OLS.summary()
+# X_ideal = X[:, [0, 1, 3, 4, 5]]
+# print(regressor_OLS._results.pvalues)
+# regressor_OLS = smodel.OLS(endog = y, exog = X_ideal).fit()
+# regressor_OLS.summary()
+# X_ideal = X[:, [0, 3, 4, 5]]
+# print(regressor_OLS._results.pvalues)
+# regressor_OLS = smodel.OLS(endog = y, exog = X_ideal).fit()
+# regressor_OLS.summary()
+# print(regressor_OLS._results.pvalues)
+# X_ideal = X[:, [0, 3, 5]]
+# regressor_OLS = smodel.OLS(endog = y, exog = X_ideal).fit()
+# regressor_OLS.summary()
+# print(regressor_OLS._results.pvalues)
+# X_ideal = X[:, [0, 3]]
+# regressor_OLS = smodel.OLS(endog = y, exog = X_ideal).fit()
+# regressor_OLS.summary()
+# print(regressor_OLS._results.pvalues)
+# regressor_OLS._results
+# =============================================================================
