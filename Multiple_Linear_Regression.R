57a7329c2b793b61ea935560044327371f9e7a3c
NTHUMan <erichsieh081@gmail.com>
2018-04-30 22:54:32 +0800

    Add backward elimination

--- Multiple_Linear_Regression.R
+++ Multiple_Linear_Regression.R
@@ -3,7 +3,7 @@ library(caTools)
 dataset = read.csv('50_Startups.csv')
 #split = sample.split(dataset$)
 
-#adjust category variable
+#adjust category variable into numeric variable
 dataset$State = factor(dataset$State, 
                        levels = c('New York', 'California', 'Florida'),
                        labels = c(1, 2, 3))
@@ -12,9 +12,18 @@ split = sample.split(dataset$Profit, SplitRatio = 0.8)
 trainset = subset(dataset, split == TRUE)
 testset = subset(dataset, split == FALSE)
 
-#regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State)
+#regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State...
 regressor = lm(formula = Profit ~ .,
                data = trainset)
 
 prediction = predict(regressor,
                      newdata = testset)
+#backward elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
+# remove the state, the p-value is too high
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+# the remain is same
